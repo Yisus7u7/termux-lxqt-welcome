a94a30e04d27b5c85e191bbde73794a196412ad4
Yisus7u7 <jesuspixel5@gmail.com>
2022-03-19 21:47:18 -0500

    fix cross-build script

--- termux-cross/build.sh
+++ termux-cross/build.sh
@@ -3,13 +3,13 @@ TERMUX_PKG_DESCRIPTION="A simple welcome app for termux-desktop-lxqt made in C++
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@Yisus7u7 <yisus7u7v@gmail.com>"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL=https://github.com/Yisus7u7/termux-lxqt-welcome
-TERMUX_PKG_GIT_BRANCH=master
-ERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg"
+TERMUX_PKG_SRCURL=https://github.com/Yisus7u7/termux-lxqt-welcome/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=78306e79550a955618be4a7e3fe63e14923a3faf58f2ccafebf6b6339cf2c96d
+TERMUX_PKG_DEPENDS="qt5-qtbase, qt5-qtsvg"
 TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qttools-cross-tools"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure(){
     "${TERMUX_PREFIX}/opt/qt/cross/bin/qmake" \
-        -spec "${TERMUX_PREFIX}/lib/qt/mkspecs/termux-cross"
+        -spec "${TERMUX_PREFIX}/lib/qt/mkspecs/termux-cross" PREFIX=${TERMUX_PREFIX}
 }
