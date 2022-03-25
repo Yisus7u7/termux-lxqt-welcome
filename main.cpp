1bffc15f7e91350b5c58bc28be6e83a8ca44f4f7
Yisus7u7 <dev.yisus@hotmail.com>
2022-03-25 17:27:51 -0500

    fix TODO

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QTranslator T;
-    T.load(":/spanish.qm");
     MainWindow w;
     w.show();
     return a.exec();
